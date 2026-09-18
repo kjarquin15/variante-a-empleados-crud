4096ce0fd969ba506890b51fcf40244615d44406
Karen Jarquín <kjarquins@miumg.edu.gt>
2026-09-17 22:22:29 -0600

    Configurar conexión con MySQL

---
+++ src/main/java/edu/umg/programacion2/sistema/conexion/ConexionBD.java
@@ -0,0 +1,16 @@
+package edu.umg.programacion2.sistema.conexion;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD {
+	private static final String URL =
+            "jdbc:mysql://localhost:3306/sistema_crud?useSSL=false&serverTimezone=UTC";
+
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "NoOlvidarQuitarParaSubirAGit";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, PASSWORD);
+    }
+}
